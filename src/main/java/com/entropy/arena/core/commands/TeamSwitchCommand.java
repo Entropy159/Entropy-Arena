e0a6f6b9b6c2108a3a16409d4b8044624b25ed6f
Entropy159 <entropy159@gmail.com>
2026-05-16 09:56:44 -0600

    Team switch notification

--- src/main/java/com/entropy/arena/core/commands/TeamSwitchCommand.java
+++ src/main/java/com/entropy/arena/core/commands/TeamSwitchCommand.java
@@ -1,6 +1,7 @@
 package com.entropy.arena.core.commands;
 
 import com.entropy.arena.api.ArenaTeam;
+import com.entropy.arena.api.Notification;
 import com.entropy.arena.api.data.ArenaData;
 import com.entropy.arena.api.gamemode.TeamGamemode;
 import com.entropy.arena.api.map.ArenaMap;
@@ -8,8 +9,10 @@ import com.entropy.arena.core.ArenaLogic;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
+import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 
 import static net.minecraft.commands.Commands.argument;
 import static net.minecraft.commands.Commands.literal;
@@ -17,16 +20,18 @@ import static net.minecraft.commands.Commands.literal;
 public class TeamSwitchCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(literal("switch").requires(ctx -> ctx.isPlayer() && ArenaData.get(ctx.getLevel()).inGame() && ArenaData.get(ctx.getLevel()).currentGamemode instanceof TeamGamemode).then(argument("team", StringArgumentType.word()).suggests(TEAM_SUGGESTIONS).executes(ctx -> {
-            if (ctx.getSource().getPlayer() != null) {
+            ServerPlayer player = ctx.getSource().getPlayer();
+            if (player != null) {
                 try {
                     ArenaTeam team = ArenaTeam.valueOf(StringArgumentType.getString(ctx, "team").toUpperCase());
                     ArenaMap map = ArenaData.get(ctx.getSource().getLevel()).currentMap;
                     if (map != null) {
                         if (map.getTeams(ctx.getSource().getLevel()).contains(team)) {
                             if (ArenaData.get(ctx.getSource().getLevel()).currentGamemode instanceof TeamGamemode teamGamemode && teamGamemode.canSwitchToTeam(ctx.getSource().getPlayer(), team)) {
-                                teamGamemode.setPlayerTeam(ctx.getSource().getPlayer(), team);
+                                teamGamemode.setPlayerTeam(player, team);
                                 teamGamemode.sendToAll();
-                                ArenaLogic.get(ctx.getSource().getLevel()).onRespawn(ctx.getSource().getPlayer());
+                                ArenaLogic.get(ctx.getSource().getLevel()).onRespawn(player);
+                                Notification.toAll(Component.translatable("arena.message.switched_team", player.getDisplayName(), team.getColoredName()).withStyle(ChatFormatting.YELLOW));
                                 return 1;
                             }
                         } else {

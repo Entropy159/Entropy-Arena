eaff539dd9b07b92c101f5de17fd38b73cbcaac1
Entropy159 <entropy159@gmail.com>
2026-05-22 13:51:01 -0600

    Respawn pos at lobby

--- src/main/java/com/entropy/arena/core/mixin/ServerPlayerMixin.java
+++ src/main/java/com/entropy/arena/core/mixin/ServerPlayerMixin.java
@@ -7,16 +7,31 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.Optional;
 
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin {
+    @Shadow
+    public abstract ServerLevel serverLevel();
+
     @ModifyReturnValue(method = "findRespawnAndUseSpawnBlock", at = @At("RETURN"))
     private static Optional<ServerPlayer.RespawnPosAngle> fix(Optional<ServerPlayer.RespawnPosAngle> original, @Local(argsOnly = true) ServerLevel level, @Local(argsOnly = true) BlockPos pos, @Local(argsOnly = true) float angle) {
         if (ArenaData.get(level).inGame() && original.isEmpty()) {
             return Optional.of(new ServerPlayer.RespawnPosAngle(pos.getBottomCenter(), angle));
+        }
+        return original;
+    }
+
+    @ModifyReturnValue(method = "getRespawnPosition", at = @At("RETURN"))
+    private BlockPos respawnPos(BlockPos original) {
+        if (!ArenaData.get(serverLevel()).inGame()) {
+            BlockPos lobbyPos = ArenaData.get(serverLevel()).lobbyPos;
+            if (lobbyPos != null) {
+                return lobbyPos;
+            }
         }
         return original;
     }
